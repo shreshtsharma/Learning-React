3c6839f85d47fa5849a63c12f2ddda9deda49025
shreshtsharma <sharmashresht7@gmail.com>
2024-08-15 16:12:38 +0530

    project

--- 08miniContext/src/components/Login.jsx
+++ 08miniContext/src/components/Login.jsx
@@ -5,7 +5,7 @@ function Login() {
     const [username,setUsername]=useState("")
     const [password, setPassword] = useState("")
 
-    const {setUser}=useContext(UserContext)
+    const {setUser}=useContext(UserContext);
 
     const handleSubmit=(e)=>{
         e.preventDefault();
